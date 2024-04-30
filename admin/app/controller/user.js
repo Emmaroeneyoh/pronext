71840f4d93a8a113d169abbcd0d70f9d32a09ab2
emmaro <emmaro@gmail.com>
2024-04-30 04:24:59 +0100

    helo

--- admin/app/controller/user.js
+++ admin/app/controller/user.js
@@ -2,6 +2,7 @@ const { contactusModel } = require("../../../user/core/db/contactus");
 const { findjobModel } = require("../../../user/core/db/find.work");
 const { hiretalentModel } = require("../../../user/core/db/hire.talent");
 const { handleError } = require("../../core/utils");
+const { adminretrieveformsmodel } = require("../model/user");
 // const moment = require("moment");
 
 const adminretrievesinglehiretalentController = async (req, res, next) => {
@@ -80,7 +81,6 @@ const admindeletefindjobController = async (req, res, next) => {
 };
 const adminretrieveallfindjobController = async (req, res, next) => {
   try {
-    
     let trainee = await findjobModel.find();
     return res.status(200).json({
       status_code: 200,
@@ -96,121 +96,40 @@ const adminretrieveallfindjobController = async (req, res, next) => {
 
 const adminretrieveformsController = async (req, res, next) => {
   try {
-
-   
     const page = req.query.page || 1;
     const limit = 10;
     let skip = (page - 1) * limit;
-    const type = req.query.type;
-    const status = req.query.status;
+    const { type, status } = req.body;
     const date = req.query.date;
-    console.log(date)
     var query = { $and: [] };
 
-    if (status && status != "") {
-      console.log(status)
-      query.$and.push({ status: status });
-    }
-  
  
+
     if (date && date != "") {
       const ndate = `${date}+00:00`;
       query.$and.push({ createdAt: ndate });
-    }
-    
-    console.log(query.$and.length)
-    if (type == "findjob") {
-      let findjobdata
-      if (query.$and.length > 0) {
-        findjobdata = await findjobModel.find(query).skip(skip).limit(limit);
-      } else {
-        findjobdata = await findjobModel.find().skip(skip).limit(limit);
-      }
-      return res.status(200).json({
-        status_code: 200,
-        status: true,
-        message: "signup process successful",
-        data: findjobdata,
-        pagination: {
-          page: page,
-          limit: 15,
-          total: findjobdata.length, // This is not the total count, it's the count of items on this page
-        },
+    } 
+    if ( !type || type.length == 0) {
+      return res.status(400).json({
+        status_code: 400,
+        status: false,
+        message: "type must not be an empty array",
       });
-    } else if (type == "hiretalent") {
-      let hiretalent
-      if (query.$and.length > 0) {
-        hiretalent = await hiretalentModel.find(query).skip(skip).limit(limit);
-      } else {
-        hiretalent = await hiretalentModel.find().skip(skip).limit(limit);
+    } 
+    if ( status && status.length > 0) {
+      query.$and.push({ status: { $in: status } });
+    } 
+    const data = { page, limit, skip, type , query}
+    let trainee = await adminretrieveformsmodel(data, res);
+    return res.status(200).json({
+      status_code: 200,
+      status: true,
+      message: "signup process successful",
+      data: trainee,
+      pagination: {
+        limit , page 
       }
-      return res.status(200).json({
-        status_code: 200,
-        status: true,
-        message: "signup process successful",
-        data: hiretalent,
-        pagination: {
-          page: page,
-          limit: 15,
-          total: hiretalent.length, // This is not the total count, it's the count of items on this page
-        },
-      });
-    } else if (type == "contactus") {
-      let contactus
-      if (query.$and.length > 0) {
-       
-        contactus = await contactusModel.find(query).skip(skip).limit(limit);
-      } else {
-        
-        contactus = await contactusModel.find().skip(skip).limit(limit);
-      }
-      return res.status(200).json({
-        status_code: 200,
-        status: true,
-        message: "signup process successful",
-        data: contactus,
-        pagination: {
-          page: page,
-          limit: 15,
-          total: contactus.length, // This is not the total count, it's the count of items on this page
-        },
-      });
-    }  else if (type == "all") {
-      const halflimit = limit / 2
-      skip = (page - 1) * halflimit;
-      let contactus 
-      let hiretalent
-      let findjob
-      if (query.$and.length > 0) {
-         contactus = await contactusModel.find(query).skip(skip).limit(halflimit);
-         hiretalent = await hiretalentModel.find(query).skip(skip).limit(halflimit);
-       findjob = await findjobModel.find(query).skip(skip).limit(halflimit);
-      } else {
-         contactus = await contactusModel.find().skip(skip).limit(halflimit);
-         hiretalent = await hiretalentModel.find().skip(skip).limit(halflimit);
-       findjob = await findjobModel.find().skip(skip).limit(halflimit);
-      }
-       
-      const formdata = [...contactus, ...hiretalent, ...findjob]
-        return res.status(200).json({
-          status_code: 200,
-          status: true,
-          message: "signup process successful",
-          data: formdata,
-          pagination: {
-            page: page,
-            limit:15
-             // This is not the total count, it's the count of items on this page
-          },
-        });
-      }
-    else {
-      return res.status(200).json({
-        status_code: 200,
-        status: true,
-        message: "form type invalid",
-      });
-    }
+    });
   } catch (error) {
     console.log(error);
     handleError(error.message)(res);
---
+++ admin/app/model/user.js
@@ -0,0 +1,73 @@
+const { contactusModel } = require("../../../user/core/db/contactus");
+const { findjobModel } = require("../../../user/core/db/find.work");
+const { hiretalentModel } = require("../../../user/core/db/hire.talent");
+
+const adminretrieveformsmodel = async (data, res) => {
+  try {
+    const { page, limit, skip, type , query } = data;
+    console.log(query.$and.length);
+    let findjobdata = []
+    let hiretalent = []
+      let contactus = []
+      let formdata = []
+      let totaldata = []
+    if (type.includes("findjob")) {
+      if (query.$and.length > 0) {
+        findjobdata = await findjobModel.find(query).skip(skip).limit(limit) 
+      } else {
+        findjobdata = await findjobModel.find().skip(skip).limit(limit);
+        }
+        totaldata.push(...findjobdata);
+    }
+
+    if (type.includes("hiretalent")) {
+      if (query.$and.length > 0) {
+        hiretalent = await hiretalentModel.find(query).skip(skip).limit(limit);
+      } else {
+        hiretalent = await hiretalentModel.find().skip(skip).limit(limit);
+        }
+        totaldata.push(...hiretalent);
+    }
+      if (type.includes("contactus")) {
+        
+      if (query.$and.length > 0) {
+        contactus = await contactusModel.find(query).skip(skip).limit(limit);
+      } else {
+          contactus = await contactusModel.find().skip(skip).limit(limit);
+          }
+          totaldata.push(...contactus);
+    } else if (type == "All") {
+      const halflimit = limit / 2;
+      skip = (page - 1) * halflimit;
+      if (query.$and.length > 0) {
+        contactus = await contactusModel
+          .find(query)
+          .skip(skip)
+          .limit(halflimit);
+        hiretalent = await hiretalentModel
+          .find(query)
+          .skip(skip)
+          .limit(halflimit);
+        findjob = await findjobModel.find(query).skip(skip).limit(halflimit);
+      } else {
+        contactus = await contactusModel.find().skip(skip).limit(halflimit);
+        hiretalent = await hiretalentModel.find().skip(skip).limit(halflimit);
+        findjob = await findjobModel.find().skip(skip).limit(halflimit);
+      }
+
+       formdata = [...contactus, ...hiretalent, ...findjob];
+      
+      }
+     
+       return totaldata
+  } catch (error) {
+    console.log(error);
+    return error.message;
+    // handleError(error.message)(res)
+  }
+};
+
+
+module.exports = {
+    adminretrieveformsmodel
+}
